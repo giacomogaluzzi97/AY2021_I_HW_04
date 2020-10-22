b3b7857126b0ddf0c5e1497389f00bcc9d2f7c5e
giacomo galuzzi <giacomo.galuzzi@mail.polimi.it>
2020-10-22 15:18:44 +0200

    created structure for isr

--- GALUZZI_GIACOMO.cydsn/Global.h
+++ GALUZZI_GIACOMO.cydsn/Global.h
@@ -5,8 +5,19 @@
 #define BYTE_TO_SEND 4
 #define TRANSMIT_BUFFER_SIZE 1+BYTE_TO_SEND+1
 
+#define PHOTOR_MUX_CHANNEL 0
+#define POTEN_MUX_CHANNEL 1
+
+#define BRIGHTNESS_THRESHOLD XXXXX
+
 #include "stdio.h"
 #include "cytypes.h"
+#include "UART.h"
+#include "ADC_DelSig.h"
+#include "Timer.h"
+#include "AMux.h"
+
+
 
 extern uint8 DataBuffer[TRANSMIT_BUFFER_SIZE];
 extern volatile uint8 PacketReadyFlag;
--- GALUZZI_GIACOMO.cydsn/InterruptRoutines.c
+++ GALUZZI_GIACOMO.cydsn/InterruptRoutines.c
@@ -4,12 +4,70 @@
 
 #include "InterruptRoutines.h"
 
+/* ======================================== */
+/* ---------------- ISR ADC --------------- */
+/* ======================================== */
+
+int32 value_digit;
+
 CY_ISR(MY_ISR_ADC)
 {
+    Timer_ReadStatusRegister();
+    
+    /* Photoresistor channel */ 
+    AMux_FastSelect(PHOTOR_MUX_CHANNEL);
+    
+    value_digit = ADC_DelSig_Read32();
+    if(value_digit < 0) value_digit = 0;
+    if(value_digit > 65535) value_digit = 65535;
+    
+    DataBuffer[1] = value_digit >> 8;
+    DataBuffer[2] = value_digit & 0xFF;
+    
+    if(value_digit <= BRIGHTNESS_THRESHOLD) /*...*/
+    
+    
+    
+    /* Potentiometer channel */ 
+    AMux_FastSelect(POTEN_MUX_CHANNEL);
+    
+    value_digit = ADC_DelSig_Read32();
+    if(value_digit < 0) value_digit = 0;
+    if(value_digit > 65535) value_digit = 65535;
+    
+    DataBuffer[3] = value_digit >> 8;
+    DataBuffer[4] = value_digit & 0xFF;
+    
+    
 }    
+
+/* ======================================== */
+/* ---------------- ISR RX ---------------- */
+/* ======================================== */
+
+uint8 ch_received;
 
 CY_ISR(MY_ISR_RX)
 {
+    if(UART_ReadRxStatus() == UART_RX_STS_FIFO_NOTEMPTY)
+    {
+        ch_received = UART_GetChar();
+        
+        switch(ch_received)
+        {
+            case 'B':
+            case 'b':
+                    /*...*/ //start all
+                    break;
+            case 'S':
+            case 's':
+                    /*...*/ //stop all
+                    break;
+            default:
+                    /*...*/ //wrong char  
+                    break;
+        }    
+    }    
 } 
 
 /* [] END OF FILE */
